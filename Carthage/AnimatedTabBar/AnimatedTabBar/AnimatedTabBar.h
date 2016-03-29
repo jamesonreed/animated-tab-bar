cd167006a89d39a808b6e8b2083df2844b84f570
Jameson Reed <jameson@freshbooks.com>
2016-03-29 09:59:29 -0400

    added project with sahred scheme

---
+++ Carthage/AnimatedTabBar/AnimatedTabBar/AnimatedTabBar.h
@@ -0,0 +1,19 @@
+//
+//  AnimatedTabBar.h
+//  AnimatedTabBar
+//
+//  Created by Jameson Reed on 2016-03-29.
+//  Copyright © 2016 freshbooks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnimatedTabBar.
+FOUNDATION_EXPORT double AnimatedTabBarVersionNumber;
+
+//! Project version string for AnimatedTabBar.
+FOUNDATION_EXPORT const unsigned char AnimatedTabBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimatedTabBar/PublicHeader.h>
+
+
